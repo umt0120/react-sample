5c04ca3267370173cb56a0f76a782e420c2e89e0
umt0120 <uemoto0120@gmail.com>
2022-10-10 21:35:59 +0900

    fix: change useState to useReducer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 interface CounterProps {
   initialValue: number;
 }
 
+type Action = "DECREMENT" | "INCREMENT" | "DOUBLE" | "RESET";
+
+// 現在の値とActionをもとに処理を行うreducer
+const reducer: (currentCount: number, action: Action) => number = (
+  currentCount: number,
+  action: Action
+) => {
+  switch (action) {
+    case "INCREMENT":
+      return currentCount + 1;
+    case "DECREMENT":
+      return currentCount - 1;
+    case "DOUBLE":
+      return currentCount * 2;
+    case "RESET":
+      return 0;
+    default:
+      return currentCount;
+  }
+};
+
 const Counter: (props: CounterProps) => JSX.Element = (props: CounterProps) => {
   const { initialValue } = props;
-  const [count, setCount] = useState(initialValue);
+  const [count, dispatch] = useReducer(reducer, initialValue);
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+      <button onClick={() => dispatch("DECREMENT")}>-</button>
+      <button onClick={() => dispatch("INCREMENT")}>+</button>
+      <button onClick={() => dispatch("DOUBLE")}>x2</button>
+      <button onClick={() => dispatch("RESET")}>RESET</button>
     </div>
   );
 };
